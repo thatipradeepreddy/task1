2657ba49813ee265ca80c33b46d9544584ed19a0
thatipradeepreddy <pradeep.thati@qylis.com>
2025-02-11 18:56:35 +0530

    added proteted route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
 import { Signup } from "./screens/signup/signup"
 import { Login } from "./screens/login/login"
+import { Home } from "./screens/home/home"
+import { UserDetails } from "./types/localStorage.types"
+import { JSX } from "react"
+
+const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
+	const storedUserDetails = localStorage.getItem("userDetails")
+	const userDetails: UserDetails | null = storedUserDetails ? JSON.parse(storedUserDetails) : null
+
+	return userDetails ? children : <Navigate to='/' replace />
+}
 
 function App() {
 	return (
@@ -8,6 +18,14 @@ function App() {
 			<Routes>
 				<Route path='/' element={<Login />} />
 				<Route path='/signup' element={<Signup />} />
+				<Route
+					path='/home'
+					element={
+						<ProtectedRoute>
+							<Home />
+						</ProtectedRoute>
+					}
+				/>
 			</Routes>
 		</Router>
 	)
